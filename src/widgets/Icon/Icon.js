a62ff54ac820068d5b2154e99293ae495725781c
steveyalcin <steven.yalcin@gmail.com>
2020-02-19 00:52:14 -0400

    The weather icons are updated according to the current weather condition

--- src/widgets/Icon/Icon.js
+++ src/widgets/Icon/Icon.js
@@ -1,15 +1,12 @@
 import styled from '@emotion/styled';
 import React from 'react';
 import Wind from './icons/Breezy-2x.png';
-import ClearNight from './icons/Clear Night-2x.png';
 import Fog from './icons/Fog-2x.png';
 import Mist from './icons/Fog-2x.png';
 import Hail from './icons/Hail-2x.png';
-import CloudyDay from './icons/Mostly Cloudy-2x.png';
+import Cloudy from './icons/Mostly Cloudy-2x.png';
 import Sunny from './icons/Mostly Sunny-2x.png';
 import ClearDay from './icons/Party Cloudy-2x.png';
-import CloudyNight from './icons/Party Cloudy Night-2x.png';
-import Cloudy from './icons/Party Cloudy-2x.png';
 import Rain from './icons/Rain-2x.png';
 import Sleet from './icons/Sleet-2x.png';
 import Snow from './icons/Snow-2x.png';
@@ -28,13 +25,10 @@ const Icon = ({ conditionSummary }) => {
   var icon = '';
   switch (conditionSummary) {
     case 'Clouds':
-      icon = CloudyDay;
-      break;
-    case 'Partly Cloudy Night':
-      icon = CloudyNight;
-      break;
-    case 'Cloudy':
       icon = Cloudy;
+      break;
+    case 'Clear':
+      icon = ClearDay;
       break;
     case 'Drizzle':
       icon = Drizzle;
@@ -44,9 +38,6 @@ const Icon = ({ conditionSummary }) => {
       break;
     case 'Clear':
       icon = ClearDay;
-      break;
-    case 'Clear Night':
-      icon = ClearNight;
       break;
     case 'Fog':
       icon = Fog;
@@ -68,6 +59,9 @@ const Icon = ({ conditionSummary }) => {
       break;
     case 'Smoke':
       icon = Smoke;
+      break;
+    case 'Sunny':
+      icon = Sunny;
       break;
     case 'Hail':
       icon = Hail;
