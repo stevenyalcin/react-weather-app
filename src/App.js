771cdffb583d1d52644bebae8c102025e0f2166e
steveyalcin <steven.yalcin@gmail.com>
2020-03-02 22:24:17 -0400

    The navbar is disabled for future state for now, and will be updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="row">
         <Sidebar />
         <div className="main">
