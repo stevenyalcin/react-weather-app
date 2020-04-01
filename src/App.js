39303e1a3129a02303210ecb7e6d850c42f13373
steveyalcin <steven.yalcin@gmail.com>
2020-03-31 21:56:06 -0300

    Unused components and folders are removed from the project directory

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ import WeatherView from './components/WeatherView/WeatherView';
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <Navbar />
       <div className="row">
-        {/* <Sidebar /> */}
         <div className="main">
           <WeatherView />
-          {/* <News /> */}
         </div>
       </div>
-      {/* <Footer /> */}
     </div>
   );
 }
